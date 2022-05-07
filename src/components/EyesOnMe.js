cf1222a6bae143285ee62ce38f335504c50e08bf
JoyTriza <wangarijoytriza@gmail.com>
2022-05-07 22:08:55 +0300

    Completed task

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from `react`;
+
+function EyesOnMe () {
+    function showFocus() {
+        console.log("Good!");
+    }
+
+    function showBlur() {
+        console.log("Hey! Eyes on me!");
+    }
+
+    return (
+        <button onFocus={showFocus} onBlur={showBlur}>
+            Eyes on me
+        </button>
+    );
+
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from `react`;
+
+function Keypad() {
+    function changesMade() {
+        console.log ("Entering Password...")
+    }
+
+    return (
+        <input type = "password" onChange={changesMade}/>
+    )
+}
+
+export default Keypad;
